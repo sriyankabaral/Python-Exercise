f3d45dfaacaeee6f92453f899c75d203009c1531
sriyankabaral18 <120296847+sriyankabaral@users.noreply.github.com>
2024-06-02 17:11:33 +0545

    Add files via upload
    KBC exercise

---
+++ KBC.py
@@ -0,0 +1,16 @@
+a="!!!Wellcome To KBC!!!"
+b=a.center(50)
+print(b)
+a=["What is the capital city of Nepal?","What is the chemical symbol for water?","Solve the following equation for (x): (3x + 5 = 14).","Who wrote the play “Romeo and Juliet?","What is the largest planet in our solar system?"]
+b=["Kathmandu","H2O","3","William Shakespeare","Jupiter"]
+prize=0
+for i in range(len(a)):
+ print("Your question is:",a[i])
+ c=input("enter your answer:")
+ if(c==b[i]):
+  prize=prize+1000
+  print("Correct answer!!")
+ else:
+  print("wrong answer")
+print("YOU WON",prize,"rupees")
+   
