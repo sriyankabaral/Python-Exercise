0a988baf891bf5e23518f223784591c46535928d
sriyankabaral18 <120296847+sriyankabaral@users.noreply.github.com>
2024-06-02 17:47:04 +0545

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -1,6 +1,6 @@
 a="!!!Wellcome To KBC!!!"
-b=a.center(50)
-print(b)
+c=a.center(50)
+print(c)
 a=["What is the capital city of Nepal?","What is the chemical symbol for water?","Solve the following equation for (x): (3x + 5 = 14).","Who wrote the play “Romeo and Juliet?","What is the largest planet in our solar system?"]
 b=["Kathmandu","H2O","3","William Shakespeare","Jupiter"]
 prize=0
