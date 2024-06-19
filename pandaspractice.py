918fdbaed85195ff02b25363f0d11314860a0d39
sriyankabaral18 <120296847+sriyankabaral@users.noreply.github.com>
2024-06-19 18:34:41 +0545

    pandaspractice.py

--- pandaspractice.py
+++ pandaspractice.py
@@ -84,8 +84,8 @@ import pandas as pd
  #{
 #dict={"keys":["k1","k2","k1","k2"],
 #"names":["john","ben","david","peter"],
-"houses":["red","blue","green","red"]}
-df=pd.DataFrame(dict)
+#"houses":["red","blue","green","red"]}
+#df=pd.DataFrame(dict)
 print(df)
 print (df.pivot(index="keys",columns="names",values="houses"))
 # dict={"keys":["k1","k2","k1","k2"],
